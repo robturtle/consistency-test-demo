89f77904a9bbd3b20f9c84e5502ef1c0c192657f
Yang Liu <jeremyrobturtle@gmail.com>
2016-10-06 13:33:37 -0400

    auto collect applications and zip them

--- client.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -jar `dirname "$0"`/kvclient/build/libs/kvclient.jar "$@"
--- server.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -jar `dirname "$0"`/kvserver/build/libs/kvserver.jar "$@"
