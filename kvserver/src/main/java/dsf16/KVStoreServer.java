47160f4f7175da3d85f3d07959f965c00356816d
Yang Liu <jeremyrobturtle@gmail.com>
2016-12-08 17:25:04 -0500

    create a buggy server

--- kvserver/src/main/java/dsf16/KVStoreServer.java
+++ kvserver/src/main/java/dsf16/KVStoreServer.java
@@ -1,9 +1,10 @@
 package dsf16;
 
+import kvstore.KVStore;
 import kvstore.KVStore.Processor;
 import org.apache.thrift.server.TServer;
-import org.apache.thrift.server.TServer.Args;
 import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.transport.TTransportException;
@@ -17,18 +18,27 @@ public class KVStoreServer {
 
   private static final Logger logger = LoggerFactory.getLogger(KVStoreServer.class);
 
-  private static Processor<KVStoreHandler> processor;
+  private static Processor<KVStore.Iface> processor;
 
   public static void main(String[] args) {
-    processor = new Processor<>(new KVStoreHandler());
-    new Thread(() -> serve(processor)).start();
+    KVStore.Iface handler;
+    // example ./kvserver 9091 buggy 1000 2000
+    if (args.length == 4 && args[1].equals("buggy")) {
+      handler = new BuggyKVStoreHandler(
+        Integer.parseInt(args[2]),
+        Integer.parseInt(args[3]));
+    } else {
+      handler = new KVStoreHandler();
+    }
+    processor = new Processor<>(handler);
+    final int port = Integer.parseInt(args[0]);
+    new Thread(() -> serve(processor, port)).start();
   }
 
-  private static void serve(Processor<KVStoreHandler> processor) {
+  private static void serve(Processor<KVStore.Iface> processor, int port) {
     try {
-      final int port = 9090;
       TServerTransport transport = new TServerSocket(port); // TODO specify from command line
-      TServer server = new TSimpleServer(new Args(transport).processor(processor));
+      TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(transport).processor(processor));
 
       logger.info(String.format("Starting kvstore server at %d ...", port));
       server.serve();
