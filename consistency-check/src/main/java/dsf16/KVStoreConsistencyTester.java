6348af6e72dcd68fe5596ec61eb2efe44ac753ae
Yang Liu <jeremyrobturtle@gmail.com>
2016-12-08 19:35:17 -0500

    refactor project structure for consistency-tester

---
+++ consistency-check/src/main/java/dsf16/KVStoreConsistencyTester.java
@@ -0,0 +1,11 @@
+package dsf16;
+
+/**
+ * Launch a test to determine whether the KVStoreServer
+ * is atomic consistent
+ */
+public class KVStoreConsistencyTester {
+  public static void main(String[] args)
+  {
+  }
+}
