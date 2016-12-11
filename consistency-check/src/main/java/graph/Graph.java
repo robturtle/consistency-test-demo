4ba8f13e992baa2c3d99668f4ba72a56c91b907c
Yang Liu <jeremyrobturtle@gmail.com>
2016-12-11 14:28:43 -0500

    Graph#DepthFirstTraversal()

--- consistency-check/src/main/java/graph/Graph.java
+++ consistency-check/src/main/java/graph/Graph.java
@@ -1,9 +1,7 @@
 package graph;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 
 public class Graph<T> {
 
@@ -47,15 +45,55 @@ public class Graph<T> {
     }
   }
 
+  @FunctionalInterface
+  public interface VertexVisitor<T, R> {
+    R visit(Map<graph.Vertex<T>, VisitStatus> visitStatusMap, graph.Vertex<T> vertex);
+  }
+
+  private enum VisitStatus { Visited, Done }
+
   private final List<Vertex<T>> vertices = new ArrayList<>();
 
   public Vertex<T> newVertex(T value) {
-    Vertex<T> v = new Vertex<T>(value);
+    Vertex<T> v = new Vertex<>(value);
     vertices.add(v);
     return v;
   }
 
   public List<Vertex<T>> getVertices() {
     return vertices;
+  }
+
+  public <R> R DepthFirstTraversal(VertexVisitor<T, R> visitor) {
+    Map<graph.Vertex<T>, VisitStatus> visitStatusMap = new HashMap<>();
+    for (Vertex<T> vertex : vertices) if (!visitStatusMap.containsKey(vertex)) {
+      R result = DFSIterating(visitStatusMap, visitor, vertex);
+      if (result != null) { return result; }
+    }
+    return null;
+  }
+
+  private <R> R DFSIterating(Map<graph.Vertex<T>, VisitStatus> visitStatusMap,
+                             VertexVisitor<T, R> visitor,
+                             graph.Vertex<T> vertex) {
+    visitStatusMap.put(vertex, VisitStatus.Visited);
+    R result = visitor.visit(visitStatusMap, vertex);
+    if (result != null) {
+      return result;
+    }
+    for (graph.Vertex<T> neighbour : vertex.neighbours()) {
+      if (visitStatusMap.get(neighbour) != VisitStatus.Done) {
+        if (visitStatusMap.get(neighbour) == VisitStatus.Visited) {
+          throw new CycleDetectedException();
+        } else {
+          result = DFSIterating(visitStatusMap, visitor, neighbour);
+          if (result != null) {
+            return result;
+          }
+        }
+      }
+    }
+    visitStatusMap.put(vertex, VisitStatus.Done);
+    return null;
   }
 }
