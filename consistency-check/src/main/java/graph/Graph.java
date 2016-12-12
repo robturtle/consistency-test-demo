22b12a6e6d8ae510cf2150214f980a1066bd48d9
Yang Liu <jeremyrobturtle@gmail.com>
2016-12-11 20:23:50 -0500

    forbid link to itself

--- consistency-check/src/main/java/graph/Graph.java
+++ consistency-check/src/main/java/graph/Graph.java
@@ -27,6 +27,9 @@ public class Graph<T> {
 
     @Override
     public void add_edge_to(graph.Vertex<T> other) {
+      if (other == this) {
+        throw new AssertionError("Cannot add edge to self!");
+      }
       adjacencies.add(other);
     }
 
